43e069f6c5ddb110d9802fb83fe0c784ac5d9af8
Wensan-Git <wensanyin2024@u.northwestern.edu>
2022-10-10 20:12:45 -0400

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="header">
+        <img src="https://clubs-cu.s3.amazonaws.com/Spectator+Publishing+Logo.png" height="60" alt="Spec Logo" />
+      </div>
+      <h1 id="top_header"><center>Spectator's Sources</center></h1>
+      <div>
+        <form>
+          <span>
+            <label class="upper_text" for="name">Source Name:   </label>
+          </span>
+          <span>
+            <input class="upper_box" type="text" id="name" />
+          </span>
+          <span>
+            <label class="upper_text" for="email">Source Email:</label>
+          </span>
+          <span>
+            <input class="upper_box" type="email" id="email" />
+          </span>
+          <button class="green_button">ADD</button>
+        </form>
+
+        <div class="whiteBoxMain">
+          <span class="dark_blue">1</span>
+          <span class="dark_blue authorName">Presbo</span>
+          <span class="email">presbo@columbia.edu</span>
+          <button class="red_button">DELETE</button>
+        </div>
+
+        <div class="whiteBoxMain">
+          <span class="dark_blue">2</span>
+          <span class="dark_blue authorName">John Jay Mouse</span>
+          <span class="email">mouse@columbia.edu</span>
+          <button class="red_button">DELETE</button>
+        </div>
+
+        <div class="whiteBoxMain">
+          <span class="dark_blue">3</span>
+          <span class="dark_blue authorName">Water Bottle Man</span>
+          <span class="email">water@columbia.edu</span>
+          <button class="red_button">DELETE</button>
+        </div>
+      </div>
     </div>
   );
 }
