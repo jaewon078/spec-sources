f8e7d0074e7148f0728253b64401ed1f12c559a1
Wensan-Git <wensanyin2024@u.northwestern.edu>
2022-11-09 20:43:57 -0500

    wednesday 11.9

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,5 +1,20 @@
+const data = require("../data")
 module.exports = {
     hello: (req, res) => {
         res.json({"message": "Hello World!"});
     },
+    get_source: (req, res) => {
+        //sources.find();
+        res.json({"message": "get_sources okay", data: data.sources})
+    },
+    add_source:(req, res) => {
+        data.sources.push(req.body.source);
+        res.json({"message": "add_source okay", data: data.sources});
+    },
+    delete_source:(req, res) => {
+        data.sources = data.sources.filter(s => s.id !== req.params.id);
+        res.json({"message": "delete_source okay", data: data.sources});
+    }
+
+
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,9 @@
 const controllers = require('../controllers/controller.js');
 const router = require('express').Router();
 
-router.route("/hello").get(controllers.hello);
+router.route("/hello").get(controllers.hello); // hello signifies an endpoint in the controller
+router.route("/api/sources").get(controllers.get_source);
+router.route("api/add_source").post(controllers.add_source);
+router.route("api/delete_source/:id").post(controllers.delete_source);
 
 module.exports = router;
