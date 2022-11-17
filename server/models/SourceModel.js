face4427639018e159cb76b21db12f2853e31586
jaewon078 <lee.jaewon078@gmail.com>
2022-11-16 20:35:19 -0500

    hello

---
+++ server/models/SourceModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// WRITE THE SCHEMA HERE
+const sourceSchema = new Schema({
+    id: { type: Number, required: true },
+    name: { type: String, required: true},
+    email: { type: String, required: true }
+});
+
+const sources = mongoose.model('spec_sources', YOUR_SCHEMA);
+
+module.exports = sources;
